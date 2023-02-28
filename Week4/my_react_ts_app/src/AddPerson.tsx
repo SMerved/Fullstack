3879a0b1c427e09e8676a16bc06205fa1a036467
Søren Merved <merver3@gmail.com>
2023-02-28 18:15:11 +0100

    erfg

---
+++ Week4/my_react_ts_app/src/AddPerson.tsx
@@ -0,0 +1,10 @@
+import { person } from "./App"
+
+const AddPerson = () => {
+    return (
+        <div>
+            <button>Add Person</button>
+        </div>
+    )
+}
+export default AddPerson
