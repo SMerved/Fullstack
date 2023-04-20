00abbb6e5d8b21e62ce34ec1f39d224d4e4c6a8c
SMerved <90383268+SMerved@users.noreply.github.com>
2023-04-20 14:08:42 +0200

    Setup

--- Week6/routes/renameRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express = require("express");
-import {getObject, createObject} from "../controllers/renameController";
-
-const router = express.Router();
-
-router.route("/").get(getObject).post(createObject);
-
-export default router;
